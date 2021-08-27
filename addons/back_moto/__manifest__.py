fff2be5ed4ef8e6b6d4cc3e39fc1acb4822ca3a3
diego <diego@tekniu.mx>
2021-08-27 13:42:33 -0500

    reporte de ventas

--- addons/back_moto/__manifest__.py
+++ addons/back_moto/__manifest__.py
@@ -9,5 +9,6 @@
              "security/ir.model.access.csv",
              "views/purchase_views.xml",
              "views/product_view.xml",
+             "views/report_view.xml",
              ],
 }
