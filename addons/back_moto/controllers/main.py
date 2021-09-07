ef21280c31726936a257f42a5afa279fd34a0dc2
diego <diego@tekniu.mx>
2021-09-07 12:36:48 -0500

    importar lista

--- addons/back_moto/controllers/main.py
+++ addons/back_moto/controllers/main.py
@@ -10,17 +10,17 @@ import json
 
 class RecibeLinea(http.Controller):
 
-    @http.route('/price_list', type='json', auth="public", methods=['POST'], csrf=False)
+    @http.route('/price_list', type='http', auth="public", methods=['POST','GET'], csrf=False)
     def pricelist(self, **kw):
-        data = json.loads(request.httprequest.data)
+        # data = json.loads(request.httprequest.data)
         headers = request.httprequest.headers
         db = headers.get('db') if 'db' in headers else False
         registry = odoo.registry(db) if db else odoo.registry()
         cr = registry.cursor()
         env = odoo.api.Environment(cr, SUPERUSER_ID, {})
-        datos = data.get('params', {})
-        id = datos.get('id')
-        lineas = datos.get('lineas')
-        env['product.pricelist'].sudo().browse(id).write(lineas)
-        env.cr.commit()
+        # datos = data.get('params', {})
+        # id = datos.get('id')
+        # lineas = datos.get('lineas')
+        # env['product.pricelist'].sudo().browse(id).write(lineas)
+        # env.cr.commit()
         return {'recibe':True}
