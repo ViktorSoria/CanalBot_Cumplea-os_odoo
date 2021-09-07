52dc7d7f6f8b74f7a1de1ff26c16599b5a546fbe
diego <diego@tekniu.mx>
2021-09-07 13:46:42 -0500

    importar lista

--- addons/back_moto/controllers/main.py
+++ addons/back_moto/controllers/main.py
@@ -4,23 +4,21 @@ from odoo import models, fields, api, http, SUPERUSER_ID
 from odoo.http import content_disposition, request
 from odoo.addons.web.controllers.main import _serialize_exception
 from odoo.tools import html_escape
+import logging
 
+_logger = logging.getLogger("Moto Control")
 import json
 
 
 class RecibeLinea(http.Controller):
 
-    @http.route('/price_list', type='http', auth="public", methods=['POST','GET'], csrf=False)
+    @http.route('/price_list', type='json', auth='user', methods=['POST'], csrf=False)
     def pricelist(self, **kw):
-        # data = json.loads(request.httprequest.data)
-        headers = request.httprequest.headers
-        db = headers.get('db') if 'db' in headers else False
-        registry = odoo.registry(db) if db else odoo.registry()
-        cr = registry.cursor()
-        env = odoo.api.Environment(cr, SUPERUSER_ID, {})
-        # datos = data.get('params', {})
-        # id = datos.get('id')
-        # lineas = datos.get('lineas')
-        # env['product.pricelist'].sudo().browse(id).write(lineas)
-        # env.cr.commit()
-        return {'recibe':True}
+        _logger.warning("entro")
+        data = json.loads(request.httprequest.data)
+        datos = data.get('params', {})
+        id = datos.get('id')
+        lineas = datos.get('lineas')
+        request.env['product.pricelist'].sudo().browse(id).write(lineas)
+        request.env.cr.commit()
+        return True
