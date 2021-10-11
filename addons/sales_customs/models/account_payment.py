fa47e9e596da86b6a2ae684a42e96d20b2164dd5
New User Name <joos_dino@hotmail.com>
2021-10-11 14:42:27 -0500

    FIX: campos cambiados a dependes y stored, para su uso en la vista de lista en filtros y agrupadores

--- addons/sales_customs/models/account_payment.py
+++ addons/sales_customs/models/account_payment.py
@@ -13,6 +13,22 @@ class AccountPaymentCustom(models.Model):
 
     seller_original = fields.Many2one('res.users', string="Vendedor en Factura", compute='get_seller_from_invoice')
 
+    @api.model
+    def create(self, vals):
+        if 'reconciled_invoice_ids' in vals:
+            seller = self.env['account.account'].browse(vals['reconciled_invoice_ids'][0]).invoice_user_id
+            if seller:
+                vals['seller_original'] = seller.id
+        return super(AccountPaymentCustom, self).create(vals)    \
+
+
+    def write(self, vals):
+        if 'reconciled_invoice_ids' in vals:
+            seller = self.env['account.account'].browse(vals['reconciled_invoice_ids'][0]).invoice_user_id
+            if seller:
+                vals['seller_original'] = seller.id
+        return super(AccountPaymentCustom, self).write(vals)
+
     def get_seller_from_invoice(self):
         for rec in self:
             rec.seller_original = rec.reconciled_invoice_ids[0].invoice_user_id if rec.reconciled_invoice_ids else None
--- addons/sales_customs/models/pos_order.py
+++ addons/sales_customs/models/pos_order.py
@@ -11,7 +11,16 @@ _log = logging.getLogger("pos_order (%s) -------> " % __name__)
 class Pos_PaymentCustom(models.Model):
     _inherit = "pos.payment"
 
-    seller = fields.Many2one('res.users', string="Vendedor", related='pos_order_id.user_id')
+    seller = fields.Many2one('res.users', string="Vendedor")
 
+    @api.model
+    def create(self, vals):
+        if 'pos_order_id' in vals:
+            seller = self.env['pos.order'].browse(vals['pos_order_id']).user_id
+            if seller:
+                vals['seller'] = seller.id
+        return super(Pos_PaymentCustom, self).create(vals)
 
-
+    def get_seller_from_order(self):
+        for rec in self:
+            rec.seller = rec.pos_order_id.user_id if rec.pos_order_id else None
