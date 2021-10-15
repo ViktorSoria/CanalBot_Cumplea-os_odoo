7b16ea11ab4c4fb9ee50a5bcc3e0ba61c8a253a7
Jeho <JoosPoison>
2021-10-15 17:36:53 -0500

    ADD: Quitar creacion y editado de productos desde las ordenes de ventas y compras

--- addons/sales_customs/__manifest__.py
+++ addons/sales_customs/__manifest__.py
@@ -3,13 +3,14 @@
     "summary": "Módulo con customizaciones de lo relacionado con ventas",
     "author": "Tekniu: Isaac, Jehosafat",
     "depends": [
-        "base", "point_of_sale", "account", "pos_pay_control", "sale_stock", "stock", "sale", "sale_management"
+        "base", "point_of_sale", "account", "pos_pay_control", "sale_stock", "stock", "sale", "sale_management", "purchase"
     ],
     "installable": True,
     "data": [
         'data/assets.xml',
         'views/pos_order_views.xml',
         'views/account_payment_views.xml',
+        'views/orders_views.xml',
     ],
     "qweb": [
         "base", "point_of_sale", "pos_pay_control"
