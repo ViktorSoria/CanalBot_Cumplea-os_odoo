d82e9109aff12702aadc8345df8b44a3b13292ab
isaac <pcgi0001x@gmail.com>
2021-11-02 18:54:44 -0600

    Pack report 3/x

--- addons/stock_picking_custom/models/stock_quant_package.py
+++ addons/stock_picking_custom/models/stock_quant_package.py
@@ -11,17 +11,15 @@ _log = logging.getLogger("stock_quant_package (%s) -------> " % __name__)
 class StockQuantPackageCustom(models.Model):
     _inherit = "stock.quant.package"
 
-    def get_empacador_name(self):
+    def get_picking_data(self):
         picking_line_ids = self.env['stock.move.line'].search([('result_package_id', '=', self.id)], limit=1)[:1]
         if not picking_line_ids:
-            return "Sin fecha de confirmación"
+            return False
         picking_id = picking_line_ids.picking_id
-        return str(picking_id.packager)
-
-    def get_picking_validate_date(self):
-        picking_line_ids = self.env['stock.move.line'].search([('result_package_id', '=', self.id)], limit=1)[:1]
-        if not picking_line_ids:
-            return "Sin fecha de confirmación"
-        picking_id = picking_line_ids.picking_id
-        return str(picking_id.date_done.date())
+        return {
+            'date_done': str(picking_id.date_done.date()),
+            'packager': str(picking_id.packager),
+            'client': str(picking_id.partner_id.name),
+            'origin': str(picking_id.origin)
+        }
 
