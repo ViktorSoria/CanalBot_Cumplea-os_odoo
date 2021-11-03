3c20fe1033171f3ce704b3194c72db375c379dbc
isaac <pcgi0001x@gmail.com>
2021-11-02 18:00:40 -0600

    Pack report 2/3 solo falta feedback

--- addons/stock_picking_custom/models/__init__.py
+++ addons/stock_picking_custom/models/__init__.py
@@ -1,2 +1,3 @@
 from . import stock_picking, stock_inventory
 from . import stock_move
+from . import stock_quant_package
--- addons/stock_picking_custom/models/stock_quant_package.py
+++ addons/stock_picking_custom/models/stock_quant_package.py
@@ -12,5 +12,16 @@ class StockQuantPackageCustom(models.Model):
     _inherit = "stock.quant.package"
 
     def get_empacador_name(self):
-        # ESTA FUNCION VA Y BUSCA EL NOMBRE DEL EMPACADOR EN LA TRANSFERENCIA DONDE FUE CREADO EL PACK
-        _log.info("Obteniendo el nombre del que empacará")
+        picking_line_ids = self.env['stock.move.line'].search([('result_package_id', '=', self.id)], limit=1)[:1]
+        if not picking_line_ids:
+            return "Sin fecha de confirmación"
+        picking_id = picking_line_ids.picking_id
+        return str(picking_id.packager)
+
+    def get_picking_validate_date(self):
+        picking_line_ids = self.env['stock.move.line'].search([('result_package_id', '=', self.id)], limit=1)[:1]
+        if not picking_line_ids:
+            return "Sin fecha de confirmación"
+        picking_id = picking_line_ids.picking_id
+        return str(picking_id.date_done.date())
+
