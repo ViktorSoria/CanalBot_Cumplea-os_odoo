802e554c6f3b70d31ee5473f7ccb5adcfec9e0f7
Manuel <ManOnof98@gmail.com>
2021-12-27 12:06:24 -0600

    Permisos modulo venta

---
+++ addons/website_prueba/__manifest__.py
@@ -0,0 +1,6 @@
+{
+    'name': 'Prueba para el sitio web principal',
+    'author': 'tekniu',
+    'installable': True,
+    'data': ['template/web.xml', 'template/assets.xml','template/no_edit.xml',],
+}
---
+++ addons/website_prueba/models/model.py
@@ -0,0 +1,7 @@
+from odoo import models, fields
+
+#class Envio(models.Model):
+#    _inherit='res.partner'
+
+#    nuevo_campo=fields.Char(string="Nuevo Campo")
+#    return True
