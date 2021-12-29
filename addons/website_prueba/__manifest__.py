8b4f041d306d357d1bcf14ab6332b132536751ae
Manuel <ManOnof98@gmail.com>
2021-12-28 18:10:35 -0600

    Permiso cambiar precio unitario

--- addons/website_prueba/__manifest__.py
+++ addons/website_prueba/__manifest__.py
@@ -2,5 +2,11 @@
     'name': 'Prueba para el sitio web principal',
     'author': 'tekniu',
     'installable': True,
-    'data': ['template/web.xml', 'template/assets.xml','template/no_edit.xml',],
+    'data': [
+             'template/web.xml',
+             'template/assets.xml',
+             'template/no_edit.xml',
+             'security/permisos.xml',
+             ],
+    'depends':['base','sale',],
 }
