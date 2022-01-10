cbb232ebcaba69ff0081a60f29bedcd24db18478
diego <diego@tekniu.mx>
2022-01-10 12:20:05 -0600

    add guia de despacho cfdi

---
+++ addons/l10n_mx_edi_stock/models/stock_move_line.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+from odoo import api, models, fields
+
+class StockMoveLine(models.Model):
+    _inherit = 'stock.move.line'
+
+    def _get_aggregated_product_quantities(self, **kwargs):
+        """Include weight in the dict of aggregated products moved
+
+        returns: dictionary {same_key_as_super: {same_values_as_super, weight: weight}, ...}
+        """
+        aggregated_move_lines = super()._get_aggregated_product_quantities(**kwargs)
+        for k, v in aggregated_move_lines.items():
+            v['weight'] = v['product_uom_rec']._compute_quantity(v['qty_done'], v['product'].uom_id) * v['product'].weight
+        return aggregated_move_lines
