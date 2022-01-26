1ad58928a70454ae0a5937ba66fd74b47681a745
isaac <pcgi0001x@gmail.com>
2022-01-26 12:30:51 -0600

    Fix limite de crédito

--- addons/ob_customer_credit_limit/models/res_partner.py
+++ addons/ob_customer_credit_limit/models/res_partner.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 from odoo import models, fields, api, _
 from odoo.exceptions import ValidationError
+import logging
+
+_log = logging.getLogger("%s ======>>>" % __name__)
 
 
 class ResPartner(models.Model):
@@ -10,11 +13,28 @@ class ResPartner(models.Model):
     credit_warning = fields.Monetary('Monto de advertencia')
     credit_blocking = fields.Monetary('Monto de Bloqueo')
     amount_due = fields.Monetary('Due Amount', compute='_compute_amount_due')
+    credit_real = fields.Monetary('Total por cobrar', help="El total de lo facturado menos el total de pagos válidos.")
+
+    def _compute_credit_real(self):
+        # _log.info("Trigger works fine! ")
+        payment_ids = self.env['account.payment'].search([('partner_id', '=', self.id), ('state', '=like', "posted")])
+        total_payments = sum(payment_ids.mapped('amount'))
+        # _log.info("_______ TOTAL PAGOS: %s " % total_payments)
+        invoice_ids_all = self.env['account.move'].search([('partner_id', '=', self.id),
+                                                       ('state', '=like', "posted")])
+        inv_out = invoice_ids_all.filtered(lambda inv: inv.move_type in ['out_invoice'])
+        total_invoices = sum(inv_out.mapped('amount_total'))
+        notas_credito = invoice_ids_all.filtered(lambda inv: inv.move_type in ['out_refund'])
+        total_notas = sum(notas_credito.mapped('amount_total'))
+        # _log.info("_______ TOTAL FACTURADO: %s Total de facturas: %s" % (total_invoices, len(invoice_ids)))
+        self.credit_real = total_invoices-total_payments-total_notas
 
     @api.depends('credit', 'debit')
     def _compute_amount_due(self):
         for rec in self:
-            rec.amount_due = rec.credit - rec.debit
+            rec._compute_credit_real()
+            # _log.info("________ DEBIT::: %s " % rec.debit)
+            rec.amount_due = rec.credit_real - rec.debit
 
     @api.constrains('credit_warning', 'credit_blocking')
     def _check_credit_amount(self):
